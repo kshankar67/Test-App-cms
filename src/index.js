0e90ca7a19eeb3fee7271277d2f4d44ba655a696
Shankar Kumar <kshankar67@users.noreply.github.com>
2022-12-16 11:31:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ import "./css/main.scss";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+var referrer=document.referrer;
+conslole.log(referrer);
+if(getUrlVars()["fbclid"] || (referrer && referrer.includes("facebook"))){
+var url = window.location.href.replace('.html','').replace("https://zeptha.netlify.app/","https://zeptha.com/");
+window.location = url;
+}
