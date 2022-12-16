5a039c6953b8e3c20fe26823c0f18c5b2ba42388
Shankar Kumar <kshankar67@users.noreply.github.com>
2022-12-16 11:27:48 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,30 @@
 // JS Goes here - ES6 supported
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", (user) => {
+    console.log('netlifyIdentity');
+    console.log(user);
+    console.log(document.location);
+    
     if (!user) {
       window.netlifyIdentity.on("login", () => {
         document.location.href = "/admin/";
       });
     }
+    
+    
   });
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+var referrer=document.referrer;
+conslole.log(referrer);
+if(getUrlVars()["fbclid"] || (referrer && referrer.includes("facebook"))){
+var url = window.location.href.replace('.html','').replace("https://zeptha.netlify.app/","https://zeptha.com/");
+window.location = url;
 }
